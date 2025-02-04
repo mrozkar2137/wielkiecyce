b5f2a4789f6cffa9ca9c3ae2c3d54212315f2964
mrozkar2137 <johnsubnautica88@gmail.com>
2025-02-05 00:49:49 +0100

    Update flappyscript.js

--- flappyscript.js
+++ flappyscript.js
@@ -8,11 +8,11 @@ const pipeUp = new Image();
 const pipeDown = new Image();
 const grass = new Image();
 
-bird.src = "https://twoja-strona.github.io/images/emo8.png";
-bg.src = "/wielkiecyce/images/bg.png";
-pipeUp.src = "/wielkiecyce/images/pipeUp.png";
-pipeDown.src = "/wielkiecyce/images/pipeDown.png";
-grass.src = "/wielkiecyce/images/grass.png";
+bird.src = "https://mrozkar2137.github.io/wielkiecyce/images/emo8.png";
+bg.src = "https://mrozkar2137.github.io/wielkiecyce/images/bg.png";
+pipeUp.src = "https://mrozkar2137.github.io/wielkiecyce/images/pipeUp.png";
+pipeDown.src = "https://mrozkar2137.github.io/wielkiecyce/images/pipeDown.png";
+grass.src = "https://mrozkar2137.github.io/wielkiecyce/images/grass.png";
 
 // Pozycja ptaka
 let birdX = 50;
@@ -43,7 +43,7 @@ function resetGame() {
 
     // Losowanie dźwięku od 1 do 5
     let randomSoundIndex = Math.floor(Math.random() * 5) + 1;
-    let deathSound = new Audio(`/wielkiecyce/sounds/death${randomSoundIndex}.mp3`);
+    let deathSound = new Audio(`https://mrozkar2137.github.io/wielkiecyce/sounds/death${randomSoundIndex}.mp3`);
     
     // Odtworzenie losowego dźwięku przegranej
     deathSound.play();
